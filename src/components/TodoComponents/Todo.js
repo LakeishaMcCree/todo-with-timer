3e7501e36a3bba14a8548c3f938a929fcc18effc
github username <lmccree215@icloud.comgit config --global user.name LakeishaMcCree>
2021-03-20 01:13:52 -0400

    passed the toggle method to each todo component in this file

--- src/components/TodoComponents/Todo.js
+++ src/components/TodoComponents/Todo.js
@@ -4,7 +4,11 @@ import React from 'react'
 
 const Todo = props => {
     return (
-        <div>
+        <div
+            key={props.todo.id}
+            onClick={event => {
+                props.toggleComplete(props.todo.id)
+            }}>
             <p>{props.todo.task}</p>
         </div>
     )
